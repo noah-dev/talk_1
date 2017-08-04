0048d7613cb798f6f1729c2918dbfdbec3bee51c
Noah-Dev <ark_code@protonmail.com>
2017-08-04 09:56:02 -0500

    Loop feature complete

--- eightball.py
+++ eightball.py
@@ -1,14 +1,21 @@
 import random
 
 prompt = "Enter your question: "
-question = input(prompt)
-response = random.randint(1,4)
 
-if response == 1:
-    print("It is certain")
-elif response == 2:
-    print("Outlook good")
-elif response == 3:
-    print("You may rely on it")
-elif response == 4:
-    print("Ask again later")
+while True:
+    question = input(prompt)
+
+    #Equivalent to question == ""
+    if not question:
+        print("Quitting...")
+        break
+
+    response = random.randint(1,4)
+    if response == 1:
+        print("It is certain")
+    elif response == 2:
+        print("Outlook good")
+    elif response == 3:
+        print("You may rely on it")
+    elif response == 4:
+        print("Ask again later")
